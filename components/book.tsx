6b847c452b04b2ba38739d53539a2c648a11930b
mrhaubrich <marcolino.haubrich@gmail.com>
2022-11-17 15:24:35 -0300

    feat: :sparkles: Adicionado Image ao inves de img

--- components/book.tsx
+++ components/book.tsx
@@ -1,19 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import { BookType } from "../pages";
+import Image from "next/image";
+import { useState } from "react";
 
 type Props = {
     book: BookType;
 }
 
 export default function Book({ book }: Props) {
+    const [ratio , setRatio] = useState(1);
+
     return (
         <div key={book.id}>
           <h2>{book.title}</h2>
           <p>{book.author}</p>
           <p>{book.description}</p>
-          <img
+          <Image
             src={book.cover}
             alt={book.title}
+            width={200}
+            height={200 / ratio}
+            unoptimized
+            onLoad={(e) => setRatio(e.currentTarget.naturalWidth / e.currentTarget.naturalHeight)}
           />
         </div>
     );
